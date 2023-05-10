7afeda87a15690c990bfb1888e5765be36a0a317
vganapati <vganapa1@swarthmore.edu>
2023-05-10 12:52:10 -0700

    modified step1 scripts

--- step1.sh
+++ step1.sh
@@ -7,15 +7,16 @@
 #SBATCH -A m2859_g          # allocation
 
 
-RUN_NUM=${1} #0080
+RUN_NUM=${1} #080
 echo $RUN_NUM
 OUTPUT_DIR=/global/cfs/cdirs/m3562/users/vidyagan/p20231/common/results/processed_may_10_2023/r${1}
 
 
 mkdir -p $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR/stdout
-srun -n 320 -c 2 dials.stills_process $MODULES/SPREAD/params_1.phil \
+srun -n 320 -c 2 dials.stills_process \
 output.output_dir=$OUTPUT_DIR \
-output.logging_dir=$OUTPUT_DIR/stdout  \
-input.glob=/global/cfs/cdirs/m3562/sf_bernina_data_p20231/assembled/$RUN_NUM/*.h5 \
+output.logging_dir=$OUTPUT_DIR/stdout \
+/global/cfs/cdirs/m3562/sf_bernina_data_p20231/assembled/$RUN_NUM/*.h5 \
+$MODULES/SPREAD/params_1.phil \
 mp.method=mpi dispatch.pre_import=True
