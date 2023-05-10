efc44c2d5a0bf494de5bd52f606a2a7c63ed99f4
vganapati <vganapa1@swarthmore.edu>
2023-05-10 16:36:47 -0700

    cctbx.xfel

--- step1_interactivenode.sh
+++ step1_interactivenode.sh
@@ -8,7 +8,7 @@ OUTPUT_DIR=/global/cfs/cdirs/m3562/users/vidyagan/p20231/common/results/processe
 
 mkdir -p $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR/stdout
-dials.stills_process \
+cctbx.xfel.process \
 output.output_dir=$OUTPUT_DIR \
 output.logging_dir=$OUTPUT_DIR/stdout \
 /global/cfs/cdirs/m3562/sf_bernina_data_p20231/assembled/$RUN_NUM/*.h5 \
