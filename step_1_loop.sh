10f3e2fe367915f0d8a0afd3bafa94c8ebe2ca39
vganapati <vganapa1@swarthmore.edu>
2023-05-10 18:02:32 -0700

    updated params_1.phil

--- step_1_loop.sh
+++ step_1_loop.sh
@@ -1,7 +1,4 @@
-for subdir in `seq -w 029 047` `seq -w 049 055`; do
-        sbatch step1.sh $subdir params_1.phil
-done
 
-for subdir in 069 070 `seq -w 074 085` `seq -w 087 108` `seq 121 139` 147 148 `seq 151 154` `seq 163 167` 169 172 173 `seq 175 182` `seq 187 188` `seq 195 205` `seq 210 214` `seq 226 230` `seq 238 240` `seq 244 256`; do
-        sbatch step1.sh $subdir params_1_69plus.phil
+for subdir in `seq -w 088 091` `seq -w 093 101` `seq -w 103 105` `seq -w 121 125` `seq -w 127 129` `seq -w 134 139` `seq -w 163 167` 169 172 173 `seq -w 175 180` 182 `seq -w 200 205` 211 212 214 228 230 238 239 245 246 247 250 253 256; do
+        sbatch step1.sh $subdir params_1.phil
 done
