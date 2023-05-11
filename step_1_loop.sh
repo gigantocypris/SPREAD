e8725b6924ea2ddec2f2bdd77f4bd59ea41d7398
vganapati <vganapa1@swarthmore.edu>
2023-05-10 18:28:07 -0700

    renaming files

--- step_1_loop.sh
+++ step_1_loop.sh
@@ -1,4 +1,4 @@
 
 for subdir in `seq -w 088 091` `seq -w 093 101` `seq -w 103 105` `seq -w 121 125` `seq -w 127 129` `seq -w 134 139` `seq -w 163 167` 169 172 173 `seq -w 175 180` 182 `seq -w 200 205` 211 212 214 228 230 238 239 245 246 247 250 253 256; do
-        sbatch $MODULES/SPREAD/step1.sh $subdir params_1.phil
+        sbatch $MODULES/SPREAD/step_1.sh $subdir params_1.phil
 done
