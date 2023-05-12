b6c07dbcd31a6129601672a286c85aac5b2a2cdb
vganapati <vganapa1@swarthmore.edu>
2023-05-11 21:51:47 -0700

    Update interactive_create_images.sh

--- interactive_create_images.sh
+++ interactive_create_images.sh
@@ -4,7 +4,7 @@ export SCRATCH_FOLDER=$SCRATCH/psii_sim/interactive
 mkdir -p $SCRATCH_FOLDER; cd $SCRATCH_FOLDER
 
 export MN_RESOLUTION=10
-export MN_CHANNELS=20
+export MN_CHANNELS=101
 
 export CCTBX_DEVICE_PER_NODE=1
 export N_START=0
